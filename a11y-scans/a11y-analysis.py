3c9ed0547c548cfb2f64c6b2f75ddb39c748e66d
Sudheesh Singanamalla <sudheesh@cs.washington.edu>
2023-04-21 03:44:51 +0000

    Update a11y-analysis for stream read and aggregate
    Signed-off-by: Sudheesh Singanamalla <sudheesh@cs.washington.edu>

--- a11y-scans/a11y-analysis.py
+++ a11y-scans/a11y-analysis.py
@@ -1,10 +1,12 @@
 from collections import defaultdict
+import csv
 
-import pandas
+# import pandas
+import modin.pandas as pd
 import swifter
 import numpy
 import matplotlib.pyplot as plt
-
+import json
 
 def cdf(data, sort_needed=True):
     n = len(data)
@@ -16,11 +18,11 @@ def cdf(data, sort_needed=True):
 
 
 def read_aggregate_information(filepath='a11y-aggregate-scan.csv'):
-    df = pandas.read_csv(filepath)
+    df = pd.read_csv(filepath)
     return df
 
-def read_detailed_information(filepath='../a11y-detailed-result.csv'):
-    df = pandas.read_csv(filepath)
+def read_detailed_information(filepath='../detailed-result-sample.csv'):
+    df = pd.read_csv(filepath)
     return df
 
 def plot_aggregate_errors_and_warnings(df):
@@ -58,8 +60,80 @@ def plot_aggregate_errors_and_warnings(df):
     ax.legend()
     plt.savefig('warnings_errors.pdf', bbox_inches='tight')
 
-def analyseErrors(df):
-    
+
+def analyseErrors(input_dataframe_filepath='a11y-detailed-result.csv'):
+    notebook_theme_counter = defaultdict(set)
+
+    print('Pass 1 - Identifying the correct notebooks to use')
+    with open(input_dataframe_filepath, 'r') as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        for i, line in enumerate(reader):
+            if i % 10000:
+                print(f'\tPass 1 - Processed {i} records')
+            elements = dict(zip(header, line))
+            notebook_name = elements['Notebook']
+            theme = elements['Theme']
+
+            notebook_theme_counter[notebook_name].add(theme)
+    print('Completed first pass')
+
+    with open('notebook_to_theme_counter.csv', 'w') as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerow(['Notebook', 'Themes', 'NumThemes'])
+        for notebook, theme_set in notebook_theme_counter.items():
+            row = [notebook, json.dumps(list(theme_set)), len(theme_set)]
+            csvwriter.writerow(row)
+
+    print('Pass 2 - Performing the correct aggregations')
+    theme_error_counter = {}
+    with open(input_dataframe_filepath, 'r') as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        for i, line in enumerate(reader):
+            if i % 10000:
+                print(f'Processed {i} records')
+            
+            elements = dict(zip(header, line))
+            id = elements['ID']
+            notebook_name = elements['Notebook']
+            theme = elements['Theme']
+            runner = elements['Runner']
+            record_type = elements['Type']
+            type_code = elements['TypeCode']
+            detail_code = elements['DetailCode']
+            selector = elements['Selector']
+
+            num_themes_for_notebook = len(notebook_theme_counter[notebook_name])
+            if num_themes_for_notebook < 6:
+                continue
+
+            if record_type not in theme_error_counter:
+                theme_error_counter[record_type] = {}
+            if theme not in theme_error_counter[record_type]:
+                theme_error_counter[record_type][theme] = {}
+            if runner not in theme_error_counter[record_type][theme]:
+                theme_error_counter[record_type][theme][runner] = {}
+            if detail_code not in theme_error_counter[record_type][theme][runner]:
+                theme_error_counter[record_type][theme][runner][detail_code] = 0
+                
+            # Increment the error count for the specific error
+            theme_error_counter[record_type][theme][runner][detail_code] = theme_error_counter[record_type][theme][runner][detail_code] + 1
+
+    print('Beginning to write the file to disk ...')
+    with open('a11y-analyze-errors-summary.csv', 'w') as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerow(['Type', 'Theme', 'Runner', 'DetailCode', 'count'])
+        for record_type, record_indexed_data in theme_error_counter.items():
+            for theme, theme_indexed_data in record_indexed_data.items():
+                for runner, runner_indexed_data in theme_indexed_data.items():
+                    for detail_code, count in runner_indexed_data.items():
+                        row = [record_type, theme, runner, detail_code, count]
+                        csvwriter.writerow(row)
+
+
+
+    # print(f"processing df of size {df.shape}")
     # print(df.columns)
     # print(df.shape)
     # print(df.describe())
@@ -72,18 +146,18 @@ def analyseErrors(df):
     # drop rows where runner is not 'axe'. just for tessting
     # axedf = df[df['Runner'] == 'axe']
     # drop the runner column
-    print(f"dropping id and selector from df of shape {df.shape}")
-    df = df.drop(columns=[ 'Selector','ID'])
+    # print(f"dropping id and selector from df of shape {df.shape}")
+    # df = df.drop(columns=[ 'Selector','ID'])
     # print the columns
     # print(axedf.columns)
     # print(axedf.sample(3))
     # drop everything where type is not error
     # axedf = axedf[axedf['Type'] == 'error']
 # group by theme. count notebooks. filter where count <6
-    nbagg = df.swifter.groupby('Notebook').swifter.agg({'Theme': 'nunique'}).reset_index()
-    nbagg = nbagg[nbagg['Theme'] == 6]
-    nbset = nbagg['Notebook']
-    df = pandas.merge(df, nbset, on=['Notebook'], how='inner')
+    # nbagg = df.groupby('Notebook').agg({'Theme': 'nunique'}).reset_index()
+    # nbagg = nbagg[nbagg['Theme'] == 6]
+    # nbset = nbagg['Notebook']
+    # df = pd.merge(df, nbset, on=['Notebook'], how='inner')
     #df=df.groupby('Theme').filter(lambda x: x['Notebook'].count() >= 6)
     # group by theme and print counts
     
@@ -105,18 +179,20 @@ def analyseErrors(df):
     # print("group by theme and detailCode and print counts for each detail code in each theme")
     # print(f"{df.groupby(['Theme', 'DetailCode'])['DetailCode'].count()}")
 # count unique detailCodes, and store detailCode, runner, count in a new df
-    df = df.swifter.groupby(['DetailCode', 'Runner','Type','Theme'])['DetailCode'].swifter.count().reset_index(name='count')
+    # df = df.groupby(['DetailCode', 'Runner','Type','Theme'])['DetailCode'].count().reset_index(name='count')
+    # print("sample of df after groupby")
+    # print(df.head(4))
     # sorting output for easy screen readability
     # sort by type, and decreasing order of count
-    df = df.swifter.sort_values(by=['Type','Runner','count'], ascending=[True,True, False])
-    df.to_csv('a11y-error-warning-notice-counts.csv', index=False)
+    # df = df.sort_values(by=['Type','Runner','count'], ascending=[True,True, False])
+    # df.to_csv('a11y-error-warning-notice-counts.csv', index=False)
     # group by DetailCode and filter for groups where there are lessthan 6 unique themes in each group
-    df.swifter.groupby('DetailCode').swifter.filter(lambda x: x['Theme'].swifter.nunique() < 6).to_csv('a11y-error-warning-notice-counts-summary.csv', index=False)
+    # df.groupby('DetailCode').filter(lambda x: x['Theme'].nunique() < 6).to_csv('a11y-error-warning-notice-counts-summary.csv', index=False)
     # print(f"a summary of df3 is {df3.shape} ")
     # print(df3)
     
     # group by detailCode, filter for entries where number of unique counts is >1
-    df.swifter.groupby('DetailCode').filter(lambda x: x['count'].nunique() > 1).to_csv('a11y-error-warning-notice-counts-diffs.csv', index=False)
+    # df.groupby('DetailCode').filter(lambda x: x['count'].nunique() > 1).to_csv('a11y-error-warning-notice-counts-diffs.csv', index=False)
     # filter for type to be error
     # df.swifter.groupby('DetailCode').swifter.filter(lambda x: x['count'].swifter.nunique() > 1).loc]
     # print(df4.shape)
@@ -153,8 +229,8 @@ def analyseErrors(df):
 
 if __name__ == '__main__':
     # df = read_aggregate_information()
-    ddf = read_detailed_information()
-    ddf = ddf.sample(n=500)
-    analyseErrors(ddf)
+    # ddf = read_detailed_information()
+    # ddf = ddf.sample(n=500)
+    analyseErrors()
     # plot_aggregate_errors_and_warnings(df)
 
